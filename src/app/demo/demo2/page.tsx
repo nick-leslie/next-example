1d5c27d1214836475ec454d81b5cb4ea1c929f33
nick-leslie <nick.leslie303@gmail.com>
2024-04-23 18:19:58 -0400

    doing stuff

---
+++ src/app/demo/demo2/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import {useEffect, useState} from "react";
+
+export default function Page() {
+    useEffect(() => {
+        console.log("example")
+    }, []);
+    return(
+        <div>
+            demo2
+        </div>
+    );
+}
